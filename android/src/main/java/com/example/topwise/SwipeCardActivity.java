8dfc4e6db7fdce0170fe957157ab04158bdd7e9b
Altamis Fattah Atmaja <altamisfattah16@gmail.com>
2024-09-29 00:27:16 +0700

    fix: ovveride

--- android/src/main/java/com/example/topwise/SwipeCardActivity.java
+++ android/src/main/java/com/example/topwise/SwipeCardActivity.java
@@ -42,6 +42,55 @@ public class SwipeCardActivity {
         void onEventFinish(String value);
     }
 
+    public void onFindMagCard(TrackData trackData) throws RemoteException {
+        // showMessage(getResources().getString(R.string.swipe_success));
+        // showMessage(getResources().getString(R.string.one_track_data) + trackData.getFirstTrackData());
+        // showMessage(getResources().getString(R.string.two_track_data) + trackData.getSecondTrackData());
+        // showMessage(getResources().getString(R.string.three_track_data) + trackData.getThirdTrackData());
+        // showMessage(getResources().getString(R.string.card_number_data) + trackData.getCardno());
+        // showMessage(getResources().getString(R.string.card_is_valid) + trackData.getExpiryDate());
+        // showMessage(getResources().getString(R.string.format_the_track_data) + trackData.getFormatTrackData());
+        // showMessage(getResources().getString(R.string.card_service_code) + trackData.getServiceCode());
+        Map<String, String> dataMap = new HashMap<>();
+
+        dataMap.put("firstTrackData", trackData.getFirstTrackData());
+        dataMap.put("secondTrackData", trackData.getSecondTrackData());
+        dataMap.put("thirdTrackData", trackData.getThirdTrackData());
+        dataMap.put("cardNo", trackData.getCardno());
+        dataMap.put("expiryDate", trackData.getExpiryDate());
+        dataMap.put("formattedTrackData", trackData.getFormatTrackData());
+        dataMap.put("serviceCode", trackData.getServiceCode());
+
+        ObjectMapper objectMapper = new ObjectMapper();
+
+        try {
+            data = objectMapper.writeValueAsString(dataMap);
+
+            Log.d("onSuccess", "onSuccess: "+objectMapper.writeValueAsString(dataMap));
+
+            new Handler(Looper.getMainLooper()).post(new Runnable() {
+                @Override
+                public void run() {
+                    if (callback != null) {
+                        callback.onEventFinish(data);
+                    }
+                }
+            });
+
+        } catch (JsonProcessingException e) {
+            data = e.toString();
+
+            new Handler(Looper.getMainLooper()).post(new Runnable() {
+                @Override
+                public void run() {
+                    if (callback != null) {
+                        callback.onEventFinish(data);
+                    }
+                }
+            });
+        }
+    }
+
     /**
      * get swiped track data
      * open instance SDK for getting magnetic stripe detector
@@ -69,8 +118,7 @@ public class SwipeCardActivity {
                             }
                         });
                     }
-
-                    @Override
+                    
                     public void onFindMagCard(TrackData trackData) throws RemoteException {
                         // showMessage(getResources().getString(R.string.swipe_success));
                         // showMessage(getResources().getString(R.string.one_track_data) + trackData.getFirstTrackData());
