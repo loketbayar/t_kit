cb1a7297c3c14c569a373714e58f49d2ac295682
Altamis Fattah Atmaja <altamisfattah16@gmail.com>
2024-09-30 03:07:20 +0700

    fix: sdk

--- android/src/main/java/com/example/topwise/SwipeCardActivity.java
+++ android/src/main/java/com/example/topwise/SwipeCardActivity.java
@@ -11,10 +11,6 @@ import com.topwise.cloudpos.aidl.magcard.EncryptMagCardListener;
 import com.topwise.cloudpos.aidl.magcard.MagCardListener;
 import com.topwise.cloudpos.aidl.magcard.TrackData;
 
-import com.topwise.cloudpos.aidl.emv.AidlCheckCardListener;
-import com.topwise.cloudpos.aidl.emv.level2.AidlEmvL2;
-import com.topwise.cloudpostest.R;
-
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 import java.util.HashMap;
@@ -25,11 +21,9 @@ public class SwipeCardActivity {
 
     private AidlMagCard magCardDev = null; // 磁条卡设备
     private boolean isSwipeCard = false;
-    private final int timeOut = 6 * 1000;
+    private final int timeOut =6 * 1000;
     private final byte keyIndex = 0x00;
     String data = "";
-    private   AidlEmvL2 aidlEmvL2 ;
-    private final static int TIME_OUT =60 * 1000;
 
     private static final String magneticStripe= "magneticStripe";
 
@@ -42,61 +36,11 @@ public class SwipeCardActivity {
         void onEventFinish(String value);
     }
 
-    public void onFindMagCard(TrackData trackData) throws RemoteException {
-        // showMessage(getResources().getString(R.string.swipe_success));
-        // showMessage(getResources().getString(R.string.one_track_data) + trackData.getFirstTrackData());
-        // showMessage(getResources().getString(R.string.two_track_data) + trackData.getSecondTrackData());
-        // showMessage(getResources().getString(R.string.three_track_data) + trackData.getThirdTrackData());
-        // showMessage(getResources().getString(R.string.card_number_data) + trackData.getCardno());
-        // showMessage(getResources().getString(R.string.card_is_valid) + trackData.getExpiryDate());
-        // showMessage(getResources().getString(R.string.format_the_track_data) + trackData.getFormatTrackData());
-        // showMessage(getResources().getString(R.string.card_service_code) + trackData.getServiceCode());
-        Map<String, String> dataMap = new HashMap<>();
-
-        dataMap.put("firstTrackData", trackData.getFirstTrackData());
-        dataMap.put("secondTrackData", trackData.getSecondTrackData());
-        dataMap.put("thirdTrackData", trackData.getThirdTrackData());
-        dataMap.put("cardNo", trackData.getCardno());
-        dataMap.put("expiryDate", trackData.getExpiryDate());
-        dataMap.put("formattedTrackData", trackData.getFormatTrackData());
-        dataMap.put("serviceCode", trackData.getServiceCode());
-
-        ObjectMapper objectMapper = new ObjectMapper();
-
-        try {
-            data = objectMapper.writeValueAsString(dataMap);
-
-            Log.d("onSuccess", "onSuccess: "+objectMapper.writeValueAsString(dataMap));
-
-            new Handler(Looper.getMainLooper()).post(new Runnable() {
-                @Override
-                public void run() {
-                    if (callback != null) {
-                        callback.onEventFinish(data);
-                    }
-                }
-            });
-
-        } catch (JsonProcessingException e) {
-            data = e.toString();
-
-            new Handler(Looper.getMainLooper()).post(new Runnable() {
-                @Override
-                public void run() {
-                    if (callback != null) {
-                        callback.onEventFinish(data);
-                    }
-                }
-            });
-        }
-    }
-
     /**
      * get swiped track data
      * open instance SDK for getting magnetic stripe detector
      */
     public void getTrackData(SwipeCardCallback callback) {
-        Log.e("Please swipe card" +e.toString() );
 
         if (magCardDev != null) {
             try {
@@ -117,55 +61,6 @@ public class SwipeCardActivity {
                                 }
                             }
                         });
-                    }
-                    
-                    public void onFindMagCard(TrackData trackData) throws RemoteException {
-                        // showMessage(getResources().getString(R.string.swipe_success));
-                        // showMessage(getResources().getString(R.string.one_track_data) + trackData.getFirstTrackData());
-                        // showMessage(getResources().getString(R.string.two_track_data) + trackData.getSecondTrackData());
-                        // showMessage(getResources().getString(R.string.three_track_data) + trackData.getThirdTrackData());
-                        // showMessage(getResources().getString(R.string.card_number_data) + trackData.getCardno());
-                        // showMessage(getResources().getString(R.string.card_is_valid) + trackData.getExpiryDate());
-                        // showMessage(getResources().getString(R.string.format_the_track_data) + trackData.getFormatTrackData());
-                        // showMessage(getResources().getString(R.string.card_service_code) + trackData.getServiceCode());
-                        Map<String, String> dataMap = new HashMap<>();
-
-                        dataMap.put("firstTrackData", trackData.getFirstTrackData());
-                        dataMap.put("secondTrackData", trackData.getSecondTrackData());
-                        dataMap.put("thirdTrackData", trackData.getThirdTrackData());
-                        dataMap.put("cardNo", trackData.getCardno());
-                        dataMap.put("expiryDate", trackData.getExpiryDate());
-                        dataMap.put("formattedTrackData", trackData.getFormatTrackData());
-                        dataMap.put("serviceCode", trackData.getServiceCode());
-
-                        ObjectMapper objectMapper = new ObjectMapper();
-
-                        try {
-                            data = objectMapper.writeValueAsString(dataMap);
-
-                            Log.d("onSuccess", "onSuccess: "+objectMapper.writeValueAsString(dataMap));
-
-                            new Handler(Looper.getMainLooper()).post(new Runnable() {
-                                @Override
-                                public void run() {
-                                    if (callback != null) {
-                                        callback.onEventFinish(data);
-                                    }
-                                }
-                            });
-
-                        } catch (JsonProcessingException e) {
-                            data = e.toString();
-
-                            new Handler(Looper.getMainLooper()).post(new Runnable() {
-                                @Override
-                                public void run() {
-                                    if (callback != null) {
-                                        callback.onEventFinish(data);
-                                    }
-                                }
-                            });
-                        }
                     }
 
                     @Override
